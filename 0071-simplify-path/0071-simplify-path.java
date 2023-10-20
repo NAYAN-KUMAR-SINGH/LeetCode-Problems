366c8e001590f91d952df2c24f04a96837c9e71e
Aditya Kumar <120032775+NAYAN-KUMAR-SINGH@users.noreply.github.com>
2023-10-20 15:28:28 +0530

    Time: 4 ms (88.85%), Space: 43.1 MB (80.91%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> s = new Stack<>();
+        StringBuffer str = new StringBuffer();
+        String[] p = path.split("/");
+        
+        for(int i = 0; i < p.length; i++){
+            if(!s.isEmpty() && p[i].equals("..")){
+                s.pop();
+            }else if(!p[i].equals("") && !p[i].equals(".") && !p[i].equals("..")){
+                s.push(p[i]);
+            }
+        }
+        
+        if(s.isEmpty()) return "/";
+        
+        while(!s.isEmpty()){
+            str.insert(0, s.pop()).insert(0, "/");
+        }
+        
+        return str.toString();
+    }
+}
