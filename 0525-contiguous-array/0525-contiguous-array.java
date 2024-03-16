9418c1fba594f4deb2229ddb3207722e964c4922
Aditya Kumar <120032775+NAYAN-KUMAR-SINGH@users.noreply.github.com>
2024-03-16 09:22:09 +0530

    Time: 7 ms (98.69%), Space: 55 MB (69.12%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        int[] sumIndex = new int[2 * nums.length + 1];
+        int maxLen = 0, sum = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i] == 1 ? 1 : -1;
+
+            if (sum == nums.length) {
+                maxLen = i + 1; // Entire array has equal number of 0s and 1s
+            } else {
+                if (sumIndex[sum] != 0) {
+                    maxLen = Math.max(maxLen, i - sumIndex[sum] + 1);
+                } else {
+                    sumIndex[sum] = i + 1;
+                }
+            }
+        }
+
+        return maxLen;
+    }
+}
