cbfda6eb06f9c801793ddfb9477e3d049b3e8549
Aditya Kumar <120032775+NAYAN-KUMAR-SINGH@users.noreply.github.com>
2024-04-13 12:00:35 +0530

    Time: 1 ms (6.75%), Space: 41.8 MB (59.32%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,14 +1,25 @@
+import java.util.Arrays;
+
 class Solution {
     public int removeElement(int[] nums, int val) {
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == val) {
+                nums[i] = 0;
                 count++;
-            } else {
-                nums[i - count] = nums[i];
             }
+        }
+        
+        Arrays.sort(nums);
+        
+        // Reversing the array manually
+        for (int i = 0; i < nums.length / 2; i++) {
+            int temp = nums[i];
+            nums[i] = nums[nums.length - i - 1];
+            nums[nums.length - i - 1] = temp;
         }
         
         return nums.length - count;
     }
 }
+
