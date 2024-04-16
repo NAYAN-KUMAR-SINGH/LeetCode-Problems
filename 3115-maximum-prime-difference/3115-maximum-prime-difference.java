3015141db6bc3fe1fc5399de64dde4bba3c05a35
Aditya Kumar <120032775+NAYAN-KUMAR-SINGH@users.noreply.github.com>
2024-04-16 21:46:39 +0530

    Time: 20 ms (22.97%), Space: 83.6 MB (15.22%) - LeetHub

--- 3115-maximum-prime-difference/3115-maximum-prime-difference.java
+++ 3115-maximum-prime-difference/3115-maximum-prime-difference.java
@@ -1,7 +1,7 @@
 class Solution {
     
     public boolean isPrime(int n){
-        for(int i = 2; i < n; i++){
+        for(int i = 2; i <= Math.sqrt(n) ; i++){
             if(n % i == 0){
                 return false;
             }
