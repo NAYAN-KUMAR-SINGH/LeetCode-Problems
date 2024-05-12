4a1b6fb1a522c48768f48dc1a8e8675306c8f620
Aditya Kumar <120032775+NAYAN-KUMAR-SINGH@users.noreply.github.com>
2024-05-12 08:07:29 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (34.75%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int s = n;
+        int f = n;
+        
+        do{
+          s = findSquare(s);
+          f = findSquare(findSquare(f));
+        }while(s != f);
+        if(s == 1){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    private int findSquare(int num){
+        int ans = 0;
+        while(num > 0){
+        int rem = num % 10;
+        ans = ans + rem * rem;
+        num = num /10;  
+        }
+        
+        return ans;
+    }
+}
