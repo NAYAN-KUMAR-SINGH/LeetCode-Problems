24dcfbf537a25e2d5bb455226ff83f2baff6c23e
Aditya Kumar <120032775+NAYAN-KUMAR-SINGH@users.noreply.github.com>
2024-05-31 11:44:01 +0530

    Time: 1 ms (100.00%), Space: 45.8 MB (22.05%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,26 +1,20 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        int j = 0;
-        int[] arr = new int[2];
-        int temp = nums[0];
-        for(int i = 0; i < nums.length - 1; i++){
-            if(nums[i] == nums[i + 1]){
-               i+=1;
-                continue;
-                
-            }
-            else if(j < 2){
-               arr[j] = nums[i];
-                j++;
+        long xor_r = 0;
+        for (int num : nums) {
+            xor_r ^= num;
+        }
+        long mask = xor_r & (-xor_r);
+
+        int groupa = 0;
+        int groupb = 0;
+        for (int num : nums) {
+            if ((num & mask) != 0) {
+                groupa ^= num;
+            } else {
+                groupb ^= num;
             }
         }
-        
-        if (j < 2 && nums[nums.length - 1] != nums[nums.length - 2]) {
-            arr[j] = nums[nums.length - 1];
-        } 
-        
-        
-        return arr;
+        return new int[]{groupa, groupb};
     }
 }
