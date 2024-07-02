3bef8b36d5771c7cc4c4d4617c6f42288d245e66
Aditya Kumar <120032775+NAYAN-KUMAR-SINGH@users.noreply.github.com>
2024-07-02 08:06:32 +0530

    Time: 8 ms (6.19%), Space: 42.6 MB (92.32%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -5,11 +5,14 @@ class Solution {
         ArrayList<Integer> list = new ArrayList<>();
        
         for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {          
+            for (int j = 0; j < nums2.length; j++) { 
+                
                 if (nums1[i] == nums2[j] && !list.contains(nums1[i])) {
+                    
                     int count1 = countOccurrences(nums1, nums1[i]);
                     int count2 = countOccurrences(nums2, nums1[i]);
                     int minCount = Math.min(count1, count2);
+                    
                     for (int k = 0; k < minCount; k++) {
                         list.add(nums1[i]);
                     }
