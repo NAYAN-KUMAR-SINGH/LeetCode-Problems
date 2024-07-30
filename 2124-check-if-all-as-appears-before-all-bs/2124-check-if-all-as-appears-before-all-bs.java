c190aa7c59b7ebffd90bf0419e7cf79ed9d66b4c
Aditya Kumar <120032775+NAYAN-KUMAR-SINGH@users.noreply.github.com>
2024-07-30 09:27:24 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (90.72%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
@@ -1,12 +1,8 @@
 class Solution {
     public boolean checkString(String s) {
-       Stack<Character> st = new Stack<>();
-        st.push(s.charAt(0));
         for(int i = 1; i < s.length(); i++){
-            if(!st.isEmpty() && st.peek() == 'b' && s.charAt(i) == 'a'){
+            if( i > 0 && s.charAt(i - 1) == 'b' && s.charAt(i) == 'a'){
                 return false;
-            }else{
-                st.push(s.charAt(i));
             }
         }
         
