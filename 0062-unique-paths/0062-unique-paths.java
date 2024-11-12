ba373c3533dce6fda4a43fac890191cfa9265aaf
Aditya Kumar <120032775+NAYAN-KUMAR-SINGH@users.noreply.github.com>
2024-11-12 10:09:45 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (50.61%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,12 +1,16 @@
-class Solution {
+public class Solution {
     public int uniquePaths(int m, int n) {
-       int totalSteps = m + n - 2;
-        int stepsDown = m - 1;  
-        long result = 1;
-        for (int i = 1; i <= stepsDown; i++) {
-            result = result * (totalSteps - (stepsDown - i)) / i;
+        int[][] grid = new int[m][n];
+        
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(i==0 || j==0)
+                    grid[i][j] = 1;
+                else
+                    grid[i][j] = grid[i][j-1] + grid[i-1][j];
+            }
         }
         
-        return (int) result;  
+        return grid[m-1][n-1];
     }
 }
